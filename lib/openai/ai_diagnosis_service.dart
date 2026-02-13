e51a6b30cc48789d08e6321fa3badb355b055ba5
godoyfernando99-sketch <godoyfernando99@gmail.com>
2026-02-13 14:08:57 -0600

    Update ai_diagnosis_service.dart

--- lib/openai/ai_diagnosis_service.dart
+++ lib/openai/ai_diagnosis_service.dart
@@ -21,8 +21,9 @@ class AiDiagnosisService {
     }
 
     final String apiKey = OpenAiConfig.apiKey;
-    final url = Uri.parse(
-      'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=$apiKey'
+      final url = Uri.parse(
+  'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=$apiKey'
+);
     );
 
     try {
